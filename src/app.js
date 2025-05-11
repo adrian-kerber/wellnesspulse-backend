31163741b7412de0a0c1b00756788bd54f5870fb
Adrian Kerber <adrianmathiaskerber@gmail.com>
2025-05-10 21:56:03 -0400

    correcao cors app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://wellnesspulse-frontend.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
